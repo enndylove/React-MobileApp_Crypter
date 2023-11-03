7b68731844321aaf9288c80fb5500f05681f0d9a
eNNdy <olya.khomy.tska.03@gmail.com>
2023-11-03 23:12:48 +0200

    new block for homepage Stack.jsx components. Styles, images, UI component

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Header from './components/main/Header';
 import HomeHero from './components/pages/home/Homehero';
 import HotNFT from './components/pages/home/Hotnft';
 import Featured from './components/pages/home/Featured';
+import Stack from './components/pages/home/Stack';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,5 +19,6 @@ root.render(
     <HomeHero/>
     <HotNFT/>
     <Featured/>
+    <Stack/>
   </React.StrictMode>
 );
