4ff797a1c9eb25cbaa13d1cefda17f82a64b892c
eNNdy <olya.khomy.tska.03@gmail.com>
2023-12-09 23:29:30 +0200

    WagmiCoofig routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Suspense } from 'react';
-import './fonts.css'
+import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { useAccount } from 'wagmi';
+
+import './fonts.css';
 import './reset.css';
 import './index.css';
 
@@ -11,40 +15,51 @@ import HomePage from './components/main/pages/Home';
 import ConnectWallet from './components/main/pages/ConnectWallet';
 import Profile from './components/main/pages/Profile';
 
+const { publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  publicClient,
+  webSocketPublicClient,
+});
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          index
-          exact
-          element={
-            <Suspense fallback={<Preloader />}>
-              <HomePage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/connectwallet"
-          element={
-            <Suspense fallback={<Preloader />}>
-              <ConnectWallet />
-            </Suspense>
-          }
-        />
-      <Route
-        path="/profile"
-        element={
-          <Suspense fallback={<Preloader />}>
-            <Profile />
-          </Suspense>
-        }
-      />
-      </Routes>
+      <WagmiConfig config={config}>
+        <Routes>
+          <Route
+            index
+            exact
+            element={
+              <Suspense fallback={<Preloader />}>
+                <HomePage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/connectwallet"
+            element={
+              <Suspense fallback={<Preloader />}>
+                <ConnectWallet />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <Suspense fallback={<Preloader />}>
+                <Profile />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </WagmiConfig>
     </BrowserRouter>
   );
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
