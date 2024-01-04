3e58fd9072bc86d713be56265d2473c106989b20
eNNdy <91780685+enndylove@users.noreply.github.com>
2024-01-04 20:32:53 +0200

    create database "users"

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
 const cors = require('cors');
-
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,12 +29,69 @@ app.post('/api/profile', async (req, res) => {
   }
 });
 
+const usersDBPath = path.join(__dirname, 'data', 'database', 'users.db');
+
 app.get('/api/profile', async (req, res) => {
   try {
     const filePath = path.join(__dirname, 'data', 'profileData.json');
     const data = await fs.readFile(filePath, 'utf8');
-    const profileData = JSON.parse(data);
+    const profileData = data ? JSON.parse(data) : {};
+    const db = new sqlite3.Database(usersDBPath);
+    db.run(`
+      CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        address TEXT,
+        addressClip TEXT,
+        balance TEXT,
+        name TEXT,
+        status TEXT,
+        tagname TEXT,
+        usdtbalance TEXT
+      )
+    `);
 
+    const existingData = await new Promise((resolve, reject) => {
+      db.get('SELECT * FROM user WHERE address = ?', [profileData.walletAddress], (err, row) => {
+        if (err) reject(err);
+        resolve(row);
+      });
+    });
+    if (existingData) {
+      const stmt = db.prepare(`
+        UPDATE user
+        SET balance = ?,
+            name = ?,
+            status = ?,
+            tagname = ?,
+            usdtbalance = ?
+        WHERE address = ?
+      `);
+      stmt.run(
+        profileData.walletBalance,
+        profileData.walletName,
+        profileData.walletStatus,
+        profileData.walletTagName,
+        profileData.walletUSDTBalance,
+        profileData.walletAddress
+      );
+      stmt.finalize();
+    } else {
+      const stmt = db.prepare(`
+        INSERT INTO user (address, addressClip, balance, name, status, tagname, usdtbalance)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+      `);
+      stmt.run(
+        profileData.walletAddress,
+        profileData.walletAddressClip,
+        profileData.walletBalance,
+        profileData.walletName,
+        profileData.walletStatus,
+        profileData.walletTagName,
+        profileData.walletUSDTBalance
+      );
+      stmt.finalize();
+    }
+    db.close();
     res.json(profileData);
   } catch (error) {
     console.error(error);
