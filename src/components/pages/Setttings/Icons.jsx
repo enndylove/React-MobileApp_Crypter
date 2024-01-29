9c09031045990fa20373bc970d66cd0924ee9e6c
eNNdy <91780685+enndylove@users.noreply.github.com>
2024-01-29 14:44:16 +0200

    icons files

---
+++ src/components/pages/Setttings/Icons.jsx
@@ -0,0 +1,77 @@
+const IconEmail =
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M14.6 19.6C13.8 19.9 12.9 20 12 20C7.6 20 4 16.4 4 12C4 7.6 7.6 4 12 4C16.4 4 20 7.6 20 12C20 12.5 20 12.2 20 12.5C19.5 16.5 16.6388 16.6433 15.5 15.2C14.6148 14.0781 15.1 12.5 16.2 9.3M13.0539 8.33156C14.7871 9.05394 15.4859 11.3338 14.6148 13.4238C13.7437 15.5139 11.6326 16.6225 9.89937 15.9002C8.16619 15.1778 7.46734 12.8979 8.33844 10.8079C9.20954 8.71787 11.3207 7.60918 13.0539 8.33156Z" stroke="#686A6C" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>,
+  IconUser =
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M20 18.5C20 20.0294 16.5714 21 12 21C7.42857 21 4 20.0294 4 18.5C4 16.2692 8 15 12 15C16 15 20 16.5 20 18.5Z" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12 12C14.4853 12 16.5 9.98528 16.5 7.5C16.5 5.01472 14.4853 3 12 3C9.51472 3 7.5 5.01472 7.5 7.5C7.5 9.98528 9.51472 12 12 12Z" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconLink =
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M12.6496 10C15.4351 13.4976 15.9925 15.1258 12.9689 18.1626C10.1513 20.9925 8.5914 20.1466 6.57176 18.3509C4.55213 16.5551 4.65767 14.9652 5.8793 12.998" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M11.3504 13C8.56489 9.50239 8.00751 7.87415 11.0311 4.8374C13.8487 2.0075 15.4086 2.85338 17.4282 4.64914C19.4479 6.44489 19.3423 8.03482 18.1207 10.002" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconTwitter =
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M19.1893 8.68435C19.1893 8.89786 19.1893 9.00461 19.1893 9.21811C19.1893 14.2354 15.4615 20 8.64497 20C6.6213 20 4.59763 19.4662 3 18.292C3.31953 18.292 3.63905 18.3987 3.85207 18.3987C5.55621 18.3987 7.15385 17.865 8.43195 16.7975C6.83432 16.7975 5.4497 15.7299 4.91716 14.1287C5.4497 14.1287 6.08876 14.1287 6.6213 13.9152C4.91716 13.5949 3.63905 11.9936 3.63905 10.2856C4.1716 10.6059 4.70414 10.7126 5.3432 10.7126C3.74556 9.64512 3.21302 7.40334 4.1716 5.69531C6.08876 8.04385 8.85799 9.43161 11.8402 9.53836C11.5207 8.25735 11.9467 6.86958 12.9053 6.01557C14.3964 4.6278 16.7396 4.6278 18.1243 6.22907C18.9763 6.01557 19.7219 5.80207 20.4675 5.26831C20.1479 6.12232 19.6154 6.86958 18.8698 7.40334C19.6154 7.29659 20.3609 7.08308 21 6.76283C20.4675 7.51009 19.8284 8.1506 19.1893 8.68435Z" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconFacebook =
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M21 12.5294C21 18.5 19 21 12 21C5 21 3 19 3 12C3 5.5 5 3 12 3C19.5 3 21 5.5 21 12.5294Z" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M10 11.5H14" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12 16.5C12 16.5 12 12.7391 12 10.3696C12 8 12.5 7.5 14.5 7.5" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconTextarea =
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M15 17L19 21L23 17" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M3 4H21" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M3 11H21" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M3 18H11" stroke="#686A6C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconSave =
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M5 12.4444L9.26035 17L19 7" stroke="#F7FBFA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconArrow =
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M3 12H21" stroke="#202025" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M13 4L21 12L13 20" stroke="#202025" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconClose =
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M6 18L18 6" stroke="#202025" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M18 18L6 6" stroke="#202025" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconCopy =
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M10 5.04705C10.0632 4.65125 10.1576 4.30403 10.2877 4C10.9688 2.40804 12.5 2 15.9208 2C21 2 22 4 22 8.0941C22 11 21.777 13.2005 20 13.7606C19.6999 13.8552 19.3555 13.9317 18.9604 14" stroke="#686A6C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M9 22C14 22 16 20.5 16 15C16 9.5 14 8 9 8C4 8 2 9.16667 2 15C2 20.8333 4 22 9 22Z" stroke="#686A6C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M7 9C7 9 7.60457 11.3954 6.5 12.5C5.39543 13.6046 3 13 3 13" stroke="#686A6C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>,
+  IconEth =
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+      <path d="M20.623 5V16.0875L29.9943 20.275L20.623 5Z" fill="white" fillOpacity="0.602" />
+      <path d="M20.6225 5L11.25 20.275L20.6225 16.0875V5Z" fill="white" />
+      <path d="M20.623 27.4595V34.9933L30.0005 22.0195L20.623 27.4595Z" fill="white" fillOpacity="0.602" />
+      <path d="M20.6225 34.9933V27.4583L11.25 22.0195L20.6225 34.9933Z" fill="white" />
+      <path d="M20.623 25.7161L29.9943 20.2748L20.623 16.0898V25.7161Z" fill="white" fillOpacity="0.2" />
+      <path d="M11.25 20.2748L20.6225 25.7161V16.0898L11.25 20.2748Z" fill="white" fillOpacity="0.602" />
+    </svg>
+
+
+export default {
+  IconEmail,
+  IconUser,
+  IconLink,
+  IconTwitter,
+  IconFacebook,
+  IconTextarea,
+
+  IconSave,
+  IconArrow,
+  IconClose,
+  IconCopy,
+
+  IconEth
+}
