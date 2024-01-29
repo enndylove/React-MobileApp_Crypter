e7a5f20ed89488f0bfffc321fc8899373ef02bf7
eNNdy <91780685+enndylove@users.noreply.github.com>
2024-01-29 14:43:09 +0200

    picture

---
+++ src/components/UI/pages/profile/Picture.jsx
@@ -0,0 +1,87 @@
+import IBanner from "../../../../image/pages/profile/banner.webp";
+
+const IconCopy =
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.33398 4.2062C8.38667 3.87637 8.46532 3.58702 8.57372 3.33366C9.1413 2.00703 10.4173 1.66699 13.268 1.66699C17.5007 1.66699 18.334 3.33366 18.334 6.74541C18.334 9.16699 18.1482 11.0007 16.6673 11.4675C16.4173 11.5463 16.1303 11.6101 15.801 11.667"
+      stroke="#202025"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7.49935 18.3337C11.666 18.3337 13.3327 17.0837 13.3327 12.5003C13.3327 7.91699 11.666 6.66699 7.49935 6.66699C3.33268 6.66699 1.66602 7.63921 1.66602 12.5003C1.66602 17.3614 3.33268 18.3337 7.49935 18.3337Z"
+      stroke="#202025"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5.83333 7.5C5.83333 7.5 6.33714 9.49619 5.41667 10.4167C4.49619 11.3371 2.5 10.8333 2.5 10.8333"
+      stroke="#202025"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>,
+  IconPlus =
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 12H18"
+        stroke="#202025"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 18L12 6"
+        stroke="#202025"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>,
+  IconUser =
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.6673 15.4167C16.6673 16.6912 13.8102 17.5 10.0007 17.5C6.19113 17.5 3.33398 16.6912 3.33398 15.4167C3.33398 13.5577 6.66732 12.5 10.0007 12.5C13.334 12.5 16.6673 13.75 16.6673 15.4167Z"
+        stroke="#686A6C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10 10C12.0711 10 13.75 8.32107 13.75 6.25C13.75 4.17893 12.0711 2.5 10 2.5C7.92893 2.5 6.25 4.17893 6.25 6.25C6.25 8.32107 7.92893 10 10 10Z"
+        stroke="#686A6C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+
+
+export default {
+  IBanner,
+
+  IconCopy,
+  IconPlus,
+  IconUser
+}
