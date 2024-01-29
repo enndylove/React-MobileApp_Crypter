09b936acc25158aed071481ee3949a94f2e0291d
eNNdy <91780685+enndylove@users.noreply.github.com>
2024-01-29 14:49:22 +0200

    export default images for choose component

--- src/components/UI/pages/connectWallet/ChooseImg.jsx
+++ src/components/UI/pages/connectWallet/ChooseImg.jsx
@@ -2,6 +2,14 @@ import Imeta from "../../../../image/pages/connectWallet/Imeta.webp";
 import Iwallet from "../../../../image/pages/connectWallet/Iwallet.webp";
 import Iether from "../../../../image/pages/connectWallet/Iether.webp";
 import Icoin from "../../../../image/pages/connectWallet/Icoin.webp";
-import useApiArrow from "../../../../image/pages/connectWallet/useApiArrow.webp";
-import Iplaceholder from "../../../../image/pages/home/homehero-placeholder.svg";
-export { Imeta, Iwallet, Iether, Icoin, useApiArrow, Iplaceholder };
+export default {
+  Imeta,
+  Iwallet,
+  Iether,
+  Icoin
+}
+export const useApiArrow =
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M3 12H21" stroke="#F7FBFA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M13 4L21 12L13 20" stroke="#F7FBFA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
